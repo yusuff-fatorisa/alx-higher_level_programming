69787a4ba5a9115cae9a58e64cea865f24725412
yusuff-fatorisa <bosun4real009@gmail.com>
2022-11-29 10:49:44 -0800

    fix output error in script 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(str(number)[-1])
+if number < 0:
+    last = f"-{str(number)[-1]}"
+    last = int(last)
+else:
+    last = int(str(number)[-1])
 if last > 5:
     print(f"Last digit of {number:d} is {last:d} and is greater than 5")
-elif last < 6 and last != 0:
-    print(f"Last digit of {number:d} is {last:d} and is less than 6 and not 0")
-else:
+elif last == 0:
     print(f"Last digit of {number:d} is {last:d} and is 0")
+else:
+    print(f"Last digit of {number:d} is {last:d} and is less than 6 and not 0")
