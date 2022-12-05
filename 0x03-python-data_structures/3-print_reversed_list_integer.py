0e58ec732ed1cc5d1fe457e80810e3f3a76a3af6
yusuff-fatorisa <bosun4real009@gmail.com>
2022-12-04 16:48:05 -0800

    fix output error in script 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for value in range((len(my_list) - 1), -1, -1):
-        print("{:d}".format(my_list[value]))
+    if len(my_list) != 0:
+        for value in range((len(my_list) - 1), -1, -1):
+            print("{:d}".format(my_list[value]))
