bc31a464497a8169b2c604622f5cf405b24f378d
yusuff-fatorisa <bosun4real009@gmail.com>
2022-12-07 19:12:36 -0800

    fix output error in file 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    from functools import reduce
     rd = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     match_list = list(roman_string.upper())
-    return sum(list(map(lambda x: rd[x], match_list)))
+    match_values = list(map(lambda x: rd[x], match_list))
+    return reduce(lambda x, y: x + y if x > y else y - x, match_values)
