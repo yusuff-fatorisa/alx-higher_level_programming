1214b3d3671317f0a645d97d73567db71e818511
yusuff-fatorisa <bosun4real009@gmail.com>
2022-12-20 04:10:45 -0800

    fix test case errors in 100-safe_print_integer_err.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as val:
-        print("Exception: {}".format(val.args[0]))
+    except Exception as e:
+        print("Exception: {}".format(e.args[0]))
         return False
