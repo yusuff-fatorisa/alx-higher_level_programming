c5a5fc9bda11fc2027039a1e7096343c1efe8fc7
yusuff-fatorisa <bosun4real009@gmail.com>
2022-12-21 14:47:09 -0800

    fix output error in 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,8 @@
 #!usr/bin/python3
 def safe_function(fct, *args):
-    import sys
-
-    a, b = args
     try:
-        return fct(a, b)
-    except Exception as e:
+        return fct(*args)
+    except (ValueError, ZeroDivisionError, IndexError, TypeError) as e:
+        import sys
         print("Exception: {}".format(e.args[0]), file=sys.stderr)
         return None
