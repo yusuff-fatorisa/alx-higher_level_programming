b23e1fb1be2119af1b56499a651dc5d4647fa7f1
yusuff-fatorisa <bosun4real009@gmail.com>
2023-01-02 13:30:53 -0800

    fixed documentaion in 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,10 +2,16 @@
 class Rectangle(object):
     """
     This class creates an instance of a Rectangle class
+    This class creates an instance of a Rectangle class
+    This class creates an instance of a Rectangle class
+    This class creates an instance of a Rectangle class
+    This class creates an instance of a Rectangle class
     """
-
+    
     def __init__(self):
         """
+        This function initializes the Rectangle object
+        This function initializes the Rectangle object
         This function initializes the Rectangle object
         """
         pass
