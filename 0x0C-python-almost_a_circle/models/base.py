01ede13a97faf04a5915a384d9b8d3859f98fd39
yusuff-fatorisa <bosun4real009@gmail.com>
2023-01-20 09:49:42 -0800

    add 'load_from_file_csv' & 'save_to_file_csv' to base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -80,6 +80,43 @@ class Base(object):
                 instances = cls.from_json_string(f.read())
             for m, n in enumerate(instances):
                 my_list.append(cls.create(**instances[m]))
-        except:
+        except Exception:
             pass
         return my_list
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ Serializes a list object to 'csv' format """
+        my_file = cls.__name__ + ".csv"
+        with open(my_file, "w", newline="") as f:
+            pen = csv.writer(f)
+            for val in list_objs:
+                if cls.__name__ == "Rectangle":
+                    pen.writerow([val.id, val.width, val.height, val.x, val.y])
+                if cls.__name__ == "Square":
+                    pen.writerow([val.id, val.size, val.x, val.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ Deserializes a csv file """
+        objs = []
+        filename = cls.__name__ + ".csv"
+        with open(filename, "r", newline="") as f:
+            reader = csv.reader(f)
+            for val in reader:
+                if cls.__name__ == "Rectangle":
+                    id = int(val[0])
+                    wd = int(val[1])
+                    hg = int(val[2])
+                    x = int(val[3])
+                    y = int(val[4])
+                    dic = {"id": id, "width": wd, "height": hg, "x": x, "y": y}
+                if cls.__name__ == "Square":
+                    id = int(val[0])
+                    size = int(val[1])
+                    x = int(val[2])
+                    y = int(val[3])
+                    dic = {"id": id, "size": size, "x": x, "y": y}
+                obj = cls.create(**dic)
+                objs.append(obj)
+            return objs
